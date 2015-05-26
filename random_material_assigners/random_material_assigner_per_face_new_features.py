639406dc73306141048830da3e5fc3f619c1502a
Tlousky <tlousky@gmail.com>
2015-05-26 16:49:47 +0300

    Updating script to get it to work with Blender 2.74.

--- random_material_assigners/random_material_assigner_per_face_new_features.py
+++ random_material_assigners/random_material_assigner_per_face_new_features.py
@@ -23,20 +23,20 @@
 #  Homepage(Wiki)    : http://bioblog3d.wordpress.com/
 #
 #  Start of project              : 2013-01-25 by Tamir Lousky
-#  Last modified                 : 2013-03-20
+#  Last modified                 : 2015-05-25
 #
-#  Acknowledgements 
+#  Acknowledgements
 #  ================
 #
 #  Blender: Patrick Boelens (helpful tuts!), CoDEmanX (useful bmesh info @ BA forums!),
-#           ni-ko-o-kin ( useful vertex groups info @ BA forums), 
+#           ni-ko-o-kin ( useful vertex groups info @ BA forums),
 #           supergra    ( great tip on how to select loose parts @ BA forums ).
 
-bl_info = {    
+bl_info = {
     "name"       : "Random Face Material Assigner",
     "author"     : "Tamir Lousky",
-    "version"    : (2, 0, 0),
-    "blender"    : (2, 66, 0),
+    "version"    : (3, 0, 1),
+    "blender"    : (2, 74, 0),
     "category"   : "Materials",
     "location"   : "3D View >> Tools",
     "wiki_url"   : "http://bioblog3d.wordpress.com/2013/03/20/random-material-assigner-v2/",
@@ -56,12 +56,12 @@ class random_mat_panel(bpy.types.Panel):
 
     def draw( self, context):                # Draw panel UI elements #
         layout = self.layout                 # Reference to panel layout object
-        
+
         props = context.scene.face_assigner  # Create reference material assigner property group
 
         col1 = layout.column()               # Create a column
         col1.label(text="Randomly assign materials to each face on the object")
-        
+
         box = layout.box()                   # Draw a box
         col2 = box.column(align=True)        # Create a column
         col2.prop( props, "rand_seed"  )     # Create randomization seed property on column
@@ -81,7 +81,7 @@ class rand_mat_assigner(bpy.types.PropertyGroup):
         ob = bpy.context.object
 
         groups = {}
-        
+
         for group in ob.vertex_groups:
             groups[str(group.index)] = []
 
@@ -99,7 +99,7 @@ class rand_mat_assigner(bpy.types.PropertyGroup):
                            object's mesh, from its list of materials (filtered by the mat_prefix)
         """
         random.seed(self.rand_seed)   # Set the randomization seed
-    
+
         all_materials = bpy.context.object.data.materials
         filtered_materials = []
 
@@ -109,7 +109,7 @@ class rand_mat_assigner(bpy.types.PropertyGroup):
                     filtered_materials.append(material) # And filter them in
         else:
             filtered_materials = all_materials          # If there's no prefix, use all materials
-        
+
         no_of_materials = len(filtered_materials)       # Count all/filtered materials on object
 
         bpy.ops.object.mode_set(mode = 'EDIT')          # Go to edit mode to create bmesh
@@ -119,30 +119,32 @@ class rand_mat_assigner(bpy.types.PropertyGroup):
 
         ## Distribute materials based on vertex groups
         if self.assign_method == 'Vertex Group':
-            
+
             vgroups = self.get_verts_and_groups()     # Get vgroups
             if vgroups and len( vgroups.keys() ) > 0: # make sure that there are actually vgroups on this mesh
-                
+
                 for vgroup in list( vgroups.keys() ):
                     # get random material index
-                    rand_mat_index  = random.randint(0, no_of_materials - 1)  
+                    rand_mat_index  = random.randint(0, no_of_materials - 1)
 
                     # Go to vertex selection mode
                     bpy.ops.mesh.select_mode(
-                        use_extend=False, 
-                        use_expand=False, 
+                        use_extend=False,
+                        use_expand=False,
                         type='VERT')
 
                     bpy.ops.mesh.select_all(action='DESELECT')  # Deselect all verts
-                    
+
                     # Select all the vertices in the vertex group
+                    bm.verts.ensure_lookup_table()
+
                     for vert in vgroups[vgroup]:
                         bm.verts[vert].select_set(True)
 
                     # Go to face selection mode
                     bm.select_mode = {'FACE'}
                     bm.select_flush(True)
-                    
+
                     # iterate over all selected faces and assign vgroup material
                     for face in bm.faces:
                         if face.select:
@@ -161,28 +163,29 @@ class rand_mat_assigner(bpy.types.PropertyGroup):
 
             for vert in vert_indices:
                 bpy.ops.mesh.select_all(action='DESELECT')  # Deselect all verts
-                
+
+                bm.verts.ensure_lookup_table()
                 bm.verts[vert].select = True
-                
+
                 # Select all verts linked to this one (on the same island or "loose part")
                 bpy.ops.mesh.select_linked( limit=False )
-                
+
                 # Go to face selection mode
                 bm.select_mode = {'FACE'}
                 bm.select_flush(True)
 
-                rand_mat_index  = random.randint(0, no_of_materials - 1)  
-            
+                rand_mat_index  = random.randint(0, no_of_materials - 1)
+
                 # iterate over all selected (linked) faces and assign material
                 for face in bm.faces:
                     if face.select:
                         face.material_index = rand_mat_index  # Assign random material to face
-                    
+
                 # remove selected vertices from list
                 for vert in bm.verts:
                     if vert.select:
                         removed = vert_indices.pop( vert_indices.index(vert.index) )
-        
+
         ob.data.update()                            # Update the mesh from the bmesh data
         bpy.ops.object.mode_set(mode = 'OBJECT')    # Return to object mode
 
@@ -193,31 +196,31 @@ class rand_mat_assigner(bpy.types.PropertyGroup):
         description = "Randomization seed",
         options     = {'ANIMATABLE'},
         update      = randomize
-    )  
+    )
 
     mat_prefix = bpy.props.StringProperty(  # Prefix to filter materials by
-        name        = "mat_prefix", 
+        name        = "mat_prefix",
         description = "Material name filter",
         default     = "",
         update      = randomize
     )
 
     items = [
-        ('Face', 'Face', ''), 
-        ('Vertex Group', 'Vertex Group', ''), 
+        ('Face', 'Face', ''),
+        ('Vertex Group', 'Vertex Group', ''),
         ('Loose Parts', 'Loose Parts', '')
     ]
 
     assign_method = bpy.props.EnumProperty( # Material distribution method
         name    = "Material distribution method",
-        items   = items, 
+        items   = items,
         default = 'Face'
     )
-    
+
 def register():
     bpy.utils.register_module(__name__)
     bpy.types.Scene.face_assigner = bpy.props.PointerProperty(type=rand_mat_assigner)
-    
+
 def unregister():
     bpy.utils.unregister_module(__name__)
     bpy.types.Scene.face_assigner = bpy.props.PointerProperty(type=rand_mat_assigner)
