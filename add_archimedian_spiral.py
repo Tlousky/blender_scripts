39e1f778158e47f94e069da585e0551e1d49c00d
Tlousky <tlousky@gmail.com>
2015-12-23 14:58:38 +0200

    Adding control over height as well

--- add_archimedian_spiral.py
+++ add_archimedian_spiral.py
@@ -2,20 +2,20 @@ import bpy
 from math import cos, sin
 S = bpy.context.scene
 
-def add_archimedian_spiral( size, length, name ):
-    z = 0
-
+def add_archimedian_spiral( size = 0.1, length = 500, height = 1, name = 'archispiral' ):
     mesh = bpy.data.meshes.new( name = name )
 
     o = bpy.data.objects.new(name, mesh)
     o.location = (0,0,0) # place at object origin
     S.objects.link( o )
 
+    z     = 0
     verts = []  
     for i in range( length ):
         angle = 0.1 * i
         x     = ( 2 * size * angle ) * cos( angle )
         y     = ( 2 * size * angle ) * sin( angle )
+        z    += i / 10000 * height
         verts.append((x,y,z))
 
     edges = []
@@ -26,4 +26,4 @@ def add_archimedian_spiral( size, length, name ):
 
     mesh.from_pydata( verts, edges, [] )
     
-add_archimedian_spiral( 0.1, 100, 'archispiral' )
+add_archimedian_spiral( 0.1, 500, 'archispiral' )
