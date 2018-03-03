e1f85a8083467ef9dd702ca68f8a234001b26a22
tlousky <tlousky@gmail.com>
2018-03-03 21:45:23 +0200

    Adding lyricsvid maker

---
+++ csv2presentation/lyricsvid.py
@@ -0,0 +1,134 @@
+import bpy
+import pandas as pd
+from os.path import basename
+from datetime import datetime as dt
+
+class timecode():
+    ''' Convert from timecode string to time object, seconds and frames '''
+    def __init__( self, timeString, fps ):
+        self.tc_string = timeString
+        self.tc_object = tc = dt.strptime( timeString, '%M:%S.%f' ).time()
+        self.seconds   = (tc.minute) * 60 + tc.second + tc.microsecond / 1000000
+        self.frame     = int( round( self.seconds * fps ) )
+
+class TextLine():
+    def __init__( self ):
+        self.text_objects = []
+
+    def create_text( self, text, start, end, x, y, seq, channel, fontsize ):
+        t = seq.new_effect( text, 'TEXT', channel, start, end )
+        t.text        = text
+        t.align_x     = 'LEFT'
+        t.align_y     = 'CENTER'
+        t.blend_type  = 'ALPHA_OVER' # 'REPLACE'
+        t.font_size   = fontsize
+        t.location[0] = x
+        t.location[1] = y
+        
+        return t
+        
+    def add_text( self, text, start, end, x, y, seq, channel = 3, fontsize = 40 ):
+        self.text_objects.append(
+            self.create_text( text, start, end, x, y, seq, channel, fontsize ) 
+        )
+        
+class Slide():
+    def __init__( self, frame_start, frame_end ):
+        self.frame_start = frame_start
+        self.frame_end   = frame_end
+        self.lines       = []
+        
+    def add_line( self, line ):
+        self.lines.append( line )
+
+class LyricsVideo():
+    def __init__( self, audioFile, markersFile, vseSequences, scn, audioChannel = 1 ):
+        self.slides        = []
+        self.audio_file    = audioFile
+        self.markers_file  = markersFile
+        self.audio_channel = audioChannel
+        self.audio = vseSequences.new_sound( 
+            basename( audioFile ), audioFile, audioChannel, 1
+        )
+        self.fps = scn.render.fps
+        
+        # Set scene to match sound file
+        scn.frame_start = self.audio.frame_start
+        scn.frame_end   = self.audio.frame_final_end
+        
+        self.markers = pd.read_csv( markersFile, sep = '\t' )       
+        
+        # Create background
+        bg = vseSequences.new_effect( 'BG', 'COLOR', 2, 1, scn.frame_end ) 
+        bg.color = (0,0,0)
+        
+        self.create_slides( vseSequences )
+
+    def normalizedXloc( self, val, xlim ):
+        ''' Normalize any value between 0-1 to the xlim provided range '''
+        return xlim[0] + ( xlim[1] - xlim[0] ) * val 
+
+    def create_slides( self, seq, yStart = 0.95, yInterval = 0.1, xlim = ( 0.025, 0.95 ) ):
+        df = self.markers
+
+        # Divide markers into slides and lines
+        track_indices = list( df[ df['Type'] == 'Track' ].index )
+
+        for i, slideIdx in enumerate( track_indices ):
+            textChannel = self.audio_channel + 1
+            slideStart   = slideIdx
+            slideEnd     = track_indices[i+1] if i < len( track_indices ) - 1 else df.index[-1]
+            slideMarkers = df.loc[ slideStart : slideEnd ]
+            lineIndices  = [slideStart] + list( slideMarkers[ slideMarkers['Type'] == 'Subclip' ].index )
+
+            slideFrameStart = timecode( df.loc[ slideStart, 'Start' ], self.fps ).frame
+            slideFrameEnd   = timecode( df.loc[ slideEnd,   'Start' ], self.fps ).frame
+            slide = Slide( slideFrameStart, slideFrameEnd )
+
+            for j, lineIdx in enumerate( lineIndices ):
+                lineStart = lineIdx
+                lineEnd   = lineIndices[j+1] - 1 if j < len( lineIndices ) - 1 else slideEnd - 1 if i < len( track_indices ) - 1 else df.index[-1]
+                lineMarkers = df.loc[ lineStart : lineEnd ]
+
+                wholeLineText = ' '.join( lineMarkers.loc[:, df.columns[0] ].as_matrix() )
+                line = TextLine()
+                textSoFar = ''
+                textChannel += 1
+                for k, markerIndex in enumerate( lineMarkers.index ):
+                    spacer  = ' ' if markerIndex != lineMarkers.index[-1] else ''
+                    text    = lineMarkers.loc[ markerIndex, df.columns[0] ] + spacer
+                    textEnd = slide.frame_end
+                    textSoFar += text
+                    if k < len( lineMarkers ) - 1:
+                        nextMarkerIdx = lineMarkers.index[ k + 1 ]
+                        textEnd = timecode( lineMarkers.loc[ nextMarkerIdx, 'Start' ], self.fps ).frame - 1
+                    line.add_text( 
+                        text    = textSoFar,
+                        start   = timecode( lineMarkers.loc[ markerIndex, 'Start' ], self.fps ).frame - 1,
+                        end     = textEnd,
+                        x       = xlim[0],
+                        y       = yStart - yInterval * j,
+                        seq     = seq,
+                        channel = textChannel
+                    )
+                    
+                slide.add_line( line )
+            
+            self.slides.append( slide )
+            
+
+
+C = bpy.context
+S = C.scene
+
+if not S.sequence_editor: S.sequence_editor_create()
+
+vse = S.sequence_editor
+seq = vse.sequences
+
+lv = LyricsVideo( 
+    audioFile    = 'E:/Drive/Hellscore/Iron Maiden Medley/Iron Maiden Medley BASS.mp3',
+    markersFile  = r'E:\Drive\Documents\Hellscore\Hellscore_songs\IronMaiden_Medely\Markers.csv',
+    vseSequences = seq,
+    scn          = S 
+)
